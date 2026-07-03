e1e2d02353694355c2631cbb547800b81aceec55
mayleneee01 <mayleneee7@gmail.com>
2026-07-03 23:30:51 +0700

    style: implement stark grid background with subtle geometric animations

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Share_Tech_Mono } from "next/font/google";
+import Background from "@/components/Background";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -33,7 +34,10 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} ${cyberFont.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <Background />
+        {children}
+      </body>
     </html>
   );
 }
